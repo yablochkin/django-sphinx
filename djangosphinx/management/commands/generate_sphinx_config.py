8b4159ac3f6e8afbff7399bb0b48236738045ca2
Wiggins <wiggins@concentricsky.com>
2011-02-09 11:55:01 -0800

    allow --all on the generate_sphinx_config command

--- djangosphinx/management/commands/generate_sphinx_config.py
+++ djangosphinx/management/commands/generate_sphinx_config.py
@@ -1,18 +1,35 @@
-from django.core.management.base import AppCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.db import models
+import itertools
+from optparse import make_option
 
 from djangosphinx.models import SphinxModelManager
 
-class Command(AppCommand):
+class Command(BaseCommand):
     help = "Prints generic configuration for any models which use a standard SphinxSearch manager."
+    option_list = BaseCommand.option_list + (
+        make_option('--all', action='store_true',default=False,dest='find_all',help='generate config for all models in all INSTALLED_APPS'),
+    )
 
     output_transaction = True
 
-    def handle_app(self, app, **options):
+    def handle(self, *args, **options):
         from djangosphinx.utils.config import generate_config_for_model
-        model_classes = [getattr(app, n) for n in dir(app) if hasattr(getattr(app, n), '_meta')]
+
+        model_classes = []
+        if options['find_all']:
+            model_classes = itertools.chain(*(models.get_models(app) for app in models.get_apps()))
+        elif len(args):
+            app_list = [models.get_app(app_label) for app_label in args]
+            for app in app_list:
+                model_classes.extend([ getattr(app, n) for n in dir(app) if hasattr(getattr(app, n), '_meta')])
+        else:
+            raise CommandError("You must specify an app name or use --all")
+
         found = 0
         for model in model_classes:
+            if getattr(model._meta, 'proxy', False) or getattr(model._meta, 'abstract', False):
+                continue
             indexes = getattr(model, '__sphinx_indexes__', [])
             for index in indexes:
                 found += 1
@@ -20,3 +37,5 @@ class Command(AppCommand):
         if found == 0:
             print "Unable to find any models in application which use standard SphinxSearch configuration."
         #return u'\n'.join(sql_create(app, self.style)).encode('utf-8')
+
+
